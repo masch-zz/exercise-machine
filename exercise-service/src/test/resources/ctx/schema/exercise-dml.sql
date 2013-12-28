be93ae274ab4b5f98f35048506a04acf8be5607f
masch <masch@localhost>
2013-12-28 14:26:18 -0300

    Renombrado de archivo exercise.sql a exercise-ddl.sql Creacion de archivo exercise-dml.sql para unit test de servicios.

---
+++ exercise-service/src/test/resources/ctx/schema/exercise-dml.sql
@@ -0,0 +1,18 @@
+INSERT INTO AUTHOR (ID, NAME, FIRST_NAME, MIDDLE_NAME, LAST_NAME) VALUES 
+		    (1, 'Wooton', 'John', NULL, 'Wooton');
+
+/************************************************************************/
+		    
+INSERT INTO LEVEL (ID, NAME) VALUES 
+		    (1, 'NIVEL 1'), 
+		    (2, 'NIVEL 2'), 
+		    (3, 'NIVEL 3'),
+		    (4, 'NIVEL 4'),
+		    (5, 'NIVEL 5');
+
+/************************************************************************/
+
+INSERT INTO EXERCISE_BASE (ID, NAME) VALUES
+            (1, 'Rebound'),
+            (2, 'All Strokes'),
+            (3, 'Single Stroke Rolls');
